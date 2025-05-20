3438b13c661a09df149a7e2dd07e533a5c832be0
amir16sp <hafizamirsaeed906@gmail.com>
2025-05-20 23:36:01 +0500

    update

--- src/app/Vedio/BgVedio.js
+++ src/app/Vedio/BgVedio.js
@@ -9,7 +9,7 @@ const DonateSection = () => {
   return (
     <div className="relative w-full mt-20 md:mt-40">
       {/* Container for responsive height */}
-      <div className="h-[100vh] md:h-[70vh] relative w-full">
+      <div className="h-[70vh] md:h-[70vh] relative w-full">
         {/* Responsive Background Image */}
         <div className="absolute inset-0 z-0">
           {/* Mobile Image */}
