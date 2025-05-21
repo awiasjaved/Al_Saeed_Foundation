0389dc0fc39ff2ba1099779d497a004baee0cc87
Awais <muhammadawaisjaved388@gmail.com>
2025-05-21 22:10:29 +0500

    update loader

--- src/app/Loader2.js
+++ src/app/Loader2.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-import LoaderImage from "./assets/images/little_mumins_5.png";
+import LoaderImage from "./assets/images/saeed.png";
 
 function Loader2({ children }) {
   const pathname = usePathname();
